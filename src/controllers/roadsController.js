3f84e882af5ed21ac5129b8fe55d0b5834bd2c71
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-02-11 22:52:16 +0200

    render about page

--- src/controllers/roadsController.js
+++ src/controllers/roadsController.js
@@ -6,4 +6,8 @@ router.get('/road-offers', (req, res) => {
     res.render('road-offers', { layout: 'roads' });
 });
 
+router.get('/about', (req, res) => {
+    res.render('about', { layout: 'about' });
+});
+
 export default router;
