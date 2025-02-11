b51432535352f1127de75ae1afefd6cc60b38e37
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-02-11 22:59:02 +0200

    render login page

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index', { layout: 'main' });
+router.get('/login', (req, res) => {
+    res.render('login', { layout: 'login' });
 });
 
 export default router;
