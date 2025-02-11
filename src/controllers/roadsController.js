90f06d5ad8078c3f891b824a55409a632d02a51d
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-02-11 23:15:58 +0200

    render details and edit pages

--- src/controllers/roadsController.js
+++ src/controllers/roadsController.js
@@ -18,4 +18,14 @@ router.get('/journey-offer', (req, res) => {
     res.render('trip-create', { layout: 'trip-create' });
 });
 
+router.get('/offer-details', (req, res) => {
+    res.render('details', { layout: 'details' });
+});
+
+router.get('/edit-offer', (req, res) => {
+    res.render('trip-edit', { layout: 'trip-edit' });
+});
+
+
+
 export default router;
