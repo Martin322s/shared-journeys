c90bce38e0bea140f4e0523fb72f34ec14ec2b1b
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-02-28 21:52:43 +0200

    add basic admin panel

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.render('index', { layout: 'main' });
 });
 
+router.get('/admin', (req, res) => {
+    res.render('admin', { layout: 'admin' });
+});
+
 export default router;
